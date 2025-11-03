f6f4575574d69d4523c3847402097378fe117b3e
iagoramon-max <iagoramon@gmail.com>
2025-11-03 01:16:26 -0300

    Update urls.py

--- estetica_agenda/agendamentos/urls.py
+++ estetica_agenda/agendamentos/urls.py
@@ -1,6 +1,9 @@
+# agendamentos/urls.py
 from django.urls import path
 from . import views
+
 app_name = 'agendamentos'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('slots/', views.slots_for_day, name='slots_for_day'),
